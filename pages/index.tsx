44a54e44384ef559002be944af998ebbd5cc7f4c
Emilio Nosthas <emilio.nosthas@popstudios.com.sv>
2023-01-07 13:13:11 -0600

    Remove Typescript warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,6 @@ interface CardData {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {/* Pull data from server - API endpoint*/}
-            // @ts-ignore
           {exploreData?.map(({img, location, distance}) => (
             <SmallCard key={img} img={img} location={location} distance={distance} />
           ))}
