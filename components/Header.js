bd898bd47515a127cd41d29d541de9f648855268
Emilio Nosthas <emilio.nosthas@popstudios.com.sv>
2023-01-09 19:41:24 -0600

    Search page

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/dist/client/router";
 
 import {
   GlobeAltIcon,
@@ -13,11 +14,12 @@ import { DateRangePicker } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
-function Header() {
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
 
   const selectionRange = {
     startDate: startDate,
@@ -35,13 +37,26 @@ function Header() {
   };
 
   const search = () => {
-    setSearchInput("");
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests: noOfGuests
+      }
+    });
   };
 
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* Left - Logo */}
-      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div
+        onClick={() => {
+          router.push("/");
+        }}
+        className='relative flex items-center h-10 cursor-pointer my-auto'
+      >
         <Image
           src='https://links.papareact.com/qd3'
           layout='fill'
@@ -55,7 +70,7 @@ function Header() {
         <input
           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400'
           type='text'
-          placeholder='Start your search'
+          placeholder={placeholder || "Start your search"}
           value={searchInput}
           onChange={e => {
             setSearchInput(e.target.value);
